9d88dda0a27989dce1471049c38cd18e3fb27e7c
prashanth_hebbar <prashanth@knobly.com>
2019-08-25 11:58:08 +0530

    added filtering code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'opal-app1';
-}
+  title = 'OPAL';
+
+
+  articles = [
+    'AI in Automotive Industry',
+    'Stochastic methodogy for 5G Cell layout',
+    'Nanotechnology and the Future of Computation, Storage and Perception',
+    'A Review on 3GPP 5G Security Aspects',
+    'Non-Orthogonal Multiple Access',
+    'Authentication for Computer Communication > An Overview of Authentication for Computer Communications',
+    'Erasure Coding for Big Data',
+    'AI Powered Society',
+    'EV – Challenges In Going Electric',
+    'Implementation of Residue Number System Based Digital Filters',
+    ' Computational Narratology > The Story Of Computational Narratology',
+    'An overview of the 5G mobile network architecture',
+    'The Essence of Quantum Computing',
+    'TCP/IP: Understanding Transport Layer Basics – Experiential Learning Series'
+    ];
+  }
+
